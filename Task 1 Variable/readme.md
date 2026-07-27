3886202f42662d4dc09b4426b67e4820ab66abf4
hassanzamzama <hassanzamzama2024@gmail.com>
2026-07-27 20:46:38 +0500

    TAsk 1

---
+++ Task 1 Variable/readme.md
@@ -0,0 +1,3 @@
+python syntax
+variable 
+print function
---
+++ Task 1 Variable/task 1.py
@@ -0,0 +1,265 @@
+# Task 1 variables
+name = "Hassan"
+name_data_type = type(name)
+print("Student Name:", name)
+print("Data Type:", name_data_type)
+
+
+
+father_name = "Amin ul haq"
+father_name_data_type = type(father_name)
+print("Father name:", father_name)
+print("Data Type:", father_name_data_type)
+
+
+
+class_ = 10
+class_data_type = type(class_)
+print("Class:", class_)
+print("Data Type:", class_data_type)
+
+
+
+age = 16
+age_data_type = type(age)
+print(" Age:", age)
+print("Data Type:", age_data_type)
+
+
+
+roll_number = 112233
+roll_number_data_type = type(roll_number)
+print("roll number:", roll_number)
+print("Data Type:", roll_number_data_type)
+
+
+
+School_name = "APS zamzama"
+School_name_data_type = type(School_name)
+print("School_name:", School_name)
+print("Data Type:", School_name_data_type)
+
+
+
+City = "Nowshera"
+City_data_type = type(City)
+print("City:", City)
+print("Data Type:", City_data_type)
+
+
+
+GPA = 2.5
+GPA_data_type = type(GPA)
+print("GPA:", GPA)
+print("Data Type:", GPA_data_type)
+
+
+
+Fav_subject = "Computer"
+Fav_subject_data_type = type(Fav_subject)
+print("Fav_subject:", Fav_subject)
+print("Data Type:",Fav_subject_data_type)
+
+
+
+hobby = "Playing Football"
+hobby_data_type = type(hobby)
+print("Hobby:", hobby)
+print("Data Type:", hobby_data_type)
+
+
+
+date_of_birth = 2-2-2009
+date_of_birth_data_type = type(date_of_birth)
+print("Date of birth:", date_of_birth)
+print("Data Type:", date_of_birth_data_type)
+
+
+
+student_id = "2026-VIIT-2"
+student_id_data_type = type(student_id)
+print("Student id:", student_id)
+print("Data Type:",student_id_data_type)
+
+
+
+adress = "House 45, Street 8"
+adress_data_type =type(adress)
+print("Adress:", adress)
+print("Data Type:", adress_data_type)
+
+
+
+province = "Khyber pakhtoon khwa"
+province_data_type = type(province)
+print("Province:",province)
+print("Data Type:", province_data_type)
+
+
+
+country = "Pakistan"
+country_data_type = type(country)
+print("Countery:", country)
+print("Data Type:", country_data_type)
+
+
+
+postal_code = "24100"
+postal_code_data_type = type(postal_code)
+print("Postal code:", postal_code)
+print("Data Type:", postal_code_data_type)
+
+
+
+email = "hassankhan321@gamil.com"
+email_data_type = type(email)
+print("Email:", email)
+print("Data Type:",email_data_type)
+
+
+
+phone_number = "03169530034"
+phone_number_data_type = type(phone_number)
+print("Phone number:", phone_number)
+print("Data Type:", phone_number_data_type)
+
+
+
+fav_seconde_subject = "English"
+fav_seconde_subject_data_type = type(fav_seconde_subject)
+print("Fav seconde subject:", fav_seconde_subject)
+print("Data Type:", father_name_data_type)
+
+
+
+fav_teacher = "Sir Ubaid"
+fav_teacher_data_type = type(fav_teacher)
+print("Fav_teacher:", fav_teacher)
+print("Data Type:", fav_teacher_data_type)
+
+
+
+attendance_percentage = "95%"
+attendance_percentage_data_type = type(attendance_percentage)
+print("Attendance Percentage:", attendance_percentage)
+print("Data Type:", attendance_percentage_data_type)
+
+
+
+fav_sport = "Football"
+fav_sport_data_type = type(fav_sport)
+print("Fav Sport:", fav_sport)
+print("Data Type:", fav_sport_data_type)
+
+
+
+fav_colour = "Black"
+fav_colour_data_type = type(fav_colour)
+print("Fav Colour:", fav_colour)
+print("Data Type:", fav_colour_data_type)
+
+
+
+fav_food = "Pullao"
+fav_food_data_type = type(fav_food)
+print("Fav Food:", fav_food)
+print("Data Type:", fav_food_data_type)
+
+
+
+fav_book = "Human Habits"
+fav_book_data_type = type(fav_book)
+print("Fav book:", fav_book)
+print("Data Type:",fav_book_data_type)
+
+
+
+future_goal = "Business"
+future_goal_data_type = type(future_goal)
+print("Future Goal:", future_goal)
+print("Data Type:", future_goal_data_type)
+
+
+
+dream_university = "Peshawer university"
+dream_university_data_type = type(dream_university)
+print("Dream university:", dream_university)
+print("Data Type:", dream_university_data_type)
+
+
+
+programing_language_learned = "Python"
+programing_language_learned_data_type = type(programing_language_learned)
+print("Programing language learned:", programing_language_learned)
+print("Data Type:", programing_language_learned_data_type)
+
+
+
+study_hours_per_day = 2
+study_hours_per_day_data_type = type(study_hours_per_day)
+print("Study hours per day:", study_hours_per_day)
+print("Data Type:", study_hours_per_day_data_type)
+
+
+
+best_friend = "Mubashir"
+best_friend_data_type = type(best_friend)
+print("Best friend:", best_friend)
+print("Data Type:", best_friend_data_type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
