850c089d78289f6764b7a7d5068edac03fbf7973
hassanzamzama <hassanzamzama2024@gmail.com>
2026-07-28 11:24:24 +0500

    task 4

---
+++ task 4 (Simple calculator)/readme.md
@@ -0,0 +1,19 @@
+Build a Simple Calculator
+
+Requirements
+Ask the user to enter the first number.
+Ask the user to enter the second number.
+Display:
+-> Sum of the numbers
+-> Difference of the numbers
+-> Product of the numbers
+-> Division of the numbers
+
+Sample Output
+Enter first number: 20
+Enter second number: 5
+
+Addition: 25.0
+Subtraction: 15.0
+Multiplication: 100.0
+Division: 4.0
---
+++ task 4 (Simple calculator)/task 4.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter first number: "))
+
+print("addition:", num1 + num2)
+print("Data type: ", type( num1 + num2))
+
+print("subtraction:", num1 - num2)
+print("Data type: ", type(num1 - num2)
+
+print("multiplication:", num1 * num2)
+print("Data type: ", type(num1 * num2))
+
+print("multiplication:", num1 * num2)
+print("Data type: ",type(num1 / num2))
